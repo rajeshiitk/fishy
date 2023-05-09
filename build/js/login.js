6a0e55af28e01368763d076541fc551d88db2345
Rajesh Choudhary <rajuvert18@gmail.com>
2023-05-09 14:28:57 +0530

    login done

--- build/js/login.js
+++ build/js/login.js
@@ -1,19 +1,31 @@
-const startApp = ()=>{
-    const loginForm = document.getElementById('login')
-const singUpForm = document.getElementById('sign-up')
-const newUser = document.getElementById('new-user')
-console.log(loginForm)
-const toggleLogin =()=>{
-    loginForm.classList.toggle('hidden')
-    loginForm.classList.toString('flex')
-    toggleSignUp();
-}
-const toggleSignUp =()=>{
-    singUpForm.classList.toggle('hidden')
-    singUpForm.classList.toString('flex')
-    toggleLogin();
-}
-loginForm.addEventListener('click', toggleLogin )
-singUpForm.addEventListener('click', toggleSignUp)
+const startApp = () => {
+    const loginForm = document.getElementById('login-form')
+    const signUpForm = document.getElementById('sign-up-form')
+    const login = document.getElementById('login')
+    const signUp = document.getElementById('sign-up')
+   
+    const toggleLogin = () => {
+        loginForm.classList.toggle('hidden')
+        loginForm.classList.toggle('flex')
+        signUpForm.classList.toggle('hidden')
+        signUpForm.classList.toggle('flex')
+        bgToggle();
+    }
+    const toggleSignUp = () => {
+        signUpForm.classList.toggle('hidden')
+        signUpForm.classList.toggle('flex')
+        loginForm.classList.toggle('hidden')
+        loginForm.classList.toggle('flex')
+        bgToggle();
+    }
+
+    const bgToggle = () => {
+        login.classList.toggle('bg-purple-900');
+        login.querySelector('.child').classList.toggle('text-white')
+        signUp.querySelector('.child').classList.toggle('text-white')
+        signUp.classList.toggle('bg-purple-900');
+    }
+    signUp.addEventListener('click', toggleLogin)
+    login.addEventListener('click', toggleSignUp)
 }
 document.addEventListener('DOMContentLoaded', startApp)
